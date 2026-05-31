3f9a4b438e349d646b5c4efa5b5bd7f5a6814cc1
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-05-31 06:25:37 -0300

    feat(routes): implement lazy loading with Suspense and Spinner fallback

--- src/app/routes/AppRoutes.jsx
+++ src/app/routes/AppRoutes.jsx
@@ -1,27 +1,31 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import Home from '../../pages/home/Home.jsx';
+import { Suspense, lazy } from 'react';
 
 import Layout from '../../shared/components/layout/Layout.jsx';
+import Spinner from '../../shared/components/ui/Spinner.jsx';
 
-import Login from '../../features/auth/pages/login/Login.jsx';
-import Register from '../../features/auth/pages/register/Register.jsx';
+const Home = lazy(() => import('../../pages/home/Home.jsx'));
+
+const Login = lazy(() => import('../../features/auth/pages/login/Login.jsx'));
+const Register = lazy(() => import('../../features/auth/pages/register/Register.jsx'));
 
 function AppRoutes() {
   return (
     <BrowserRouter>
-      <Routes>
-        {/* Rotas públicas, sem layout global */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+      <Suspense fallback={<Spinner />}>
+        <Routes>
+          {/* Rotas públicas, sem layout global */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
 
-        {/* Rotas com layout global */}
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
+          {/* Rotas com layout global */}
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
 
-          {/* Rotas protegidas */}
-        </Route>
-      </Routes>
+            {/* Rotas protegidas */}
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
