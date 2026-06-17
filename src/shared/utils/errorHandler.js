186156606940ce69406c28161c10f844a849038e
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-06-17 13:51:21 -0300

    feat(errorHandler.js): implement centralized error handling utility

---
+++ src/shared/utils/errorHandler.js
@@ -0,0 +1,46 @@
+export default function errorHandler(error) {
+  console.error('Erro no errorHandler:', error);
+
+  if (error.type === 'network') {
+    return { message: 'Erro de conexão. Verifique a internet.' };
+  }
+
+  if (error.type === 'api') {
+    switch (error.status) {
+      case 400:
+        return {
+          message: 'Problema no envio dos dados, a solicitação está incorreta.',
+        };
+      case 401:
+        return {
+          message: 'Requer autenticação, é necessário fazer login.',
+          action: 'OPEN_LOGIN',
+        };
+      case 403:
+        return {
+          message: 'Acesso não autorizado para este recurso.',
+        };
+      case 404:
+        return {
+          message: 'Dado não encontrado, não existe ou foi removido.',
+        };
+      case 429:
+        return {
+          message:
+            'Foram feitas muitas requisições em um curto período, aguarde um instante para poder prosseguir.',
+        };
+      case 500:
+        return {
+          message: 'Erro interno do servidor. Aguarde um instante e tente novamente.',
+        };
+
+      default:
+        return {
+          message:
+            'Erro ao buscar dados. Problemas no navegador, nas informações enviadas ou houve falha no processamento do servidor.',
+        };
+    }
+  }
+
+  return { message: 'Erro inesperado (desconhecido).' };
+}
