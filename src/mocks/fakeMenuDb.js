ca20cd97373fbc7acf1c5fe5e05e059cd0fe2047
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-06-17 13:55:20 -0300

    feat(fakeMenuDb.js): add mock data for menu items

---
+++ src/mocks/fakeMenuDb.js
@@ -0,0 +1,185 @@
+export const itemsMenu = [
+  // verduras e legumes
+  {
+    inventoryLotId: 1,
+    productName: 'Abóbora',
+    category: 'verduras-legumes',
+    qtyAvailable: 10,
+    partnerName: 'Mercado 1',
+    availableUntil: new Date().toISOString(),
+  },
+  {
+    inventoryLotId: 2,
+    productName: 'Espinafre',
+    category: 'verduras-legumes',
+    qtyAvailable: 10,
+    partnerName: 'Mercado 2',
+    availableUntil: new Date().toISOString(),
+  },
+  {
+    inventoryLotId: 3,
+    productName: 'Cenoura',
+    category: 'verduras-legumes',
+    qtyAvailable: 10,
+    partnerName: 'Mercado 3',
+    availableUntil: new Date().toISOString(),
+  },
+
+  // carboidratos
+  {
+    inventoryLotId: 4,
+    productName: 'Mandioca',
+    category: 'carboidratos',
+    qtyAvailable: 20,
+    partnerName: 'Mercado 1',
+    availableUntil: new Date().toISOString(),
+  },
+  {
+    inventoryLotId: 5,
+    productName: 'Batata',
+    category: 'carboidratos',
+    qtyAvailable: 20,
+    partnerName: 'Mercado 2',
+    availableUntil: new Date().toISOString(),
+  },
+  {
+    inventoryLotId: 6,
+    productName: 'Macarrão integral',
+    category: 'carboidratos',
+    qtyAvailable: 20,
+    partnerName: 'Mercado 3',
+    availableUntil: new Date().toISOString(),
+  },
+
+  // leguminosas e oleaginosas
+  {
+    inventoryLotId: 7,
+    productName: 'Grão de bico',
+    category: 'leguminosas-oleaginosas',
+    qtyAvailable: 25,
+    partnerName: 'Mercado 1',
+    availableUntil: new Date().toISOString(),
+  },
+  {
+    inventoryLotId: 8,
+    productName: 'Feijão preto',
+    category: 'leguminosas-oleaginosas',
+    qtyAvailable: 30,
+    partnerName: 'Mercado 2',
+    availableUntil: new Date().toISOString(),
+  },
+  {
+    inventoryLotId: 9,
+    productName: 'Linhaça',
+    category: 'leguminosas-oleaginosas',
+    qtyAvailable: 30,
+    partnerName: 'Mercado 3',
+    availableUntil: new Date().toISOString(),
+  },
+
+  // leites e derivados
+  {
+    inventoryLotId: 10,
+    productName: 'Leite de soja',
+    category: 'leites-derivados',
+    qtyAvailable: 15,
+    partnerName: 'Mercado 1',
+    availableUntil: new Date().toISOString(),
+  },
+  {
+    inventoryLotId: 11,
+    productName: 'Queijo muçarela',
+    category: 'leites-derivados',
+    qtyAvailable: 12,
+    partnerName: 'Mercado 2',
+    availableUntil: new Date().toISOString(),
+  },
+  {
+    inventoryLotId: 12,
+    productName: 'Leite integral',
+    category: 'leites-derivados',
+    qtyAvailable: 18,
+    partnerName: 'Mercado 3',
+    availableUntil: new Date().toISOString(),
+  },
+
+  // carnes, ovos e peixes
+  {
+    inventoryLotId: 13,
+    productName: 'Peito de frango',
+    category: 'carnes-ovos-peixes',
+    qtyAvailable: 18,
+    partnerName: 'Mercado 1',
+    availableUntil: new Date().toISOString(),
+  },
+  {
+    inventoryLotId: 14,
+    productName: 'Carne de soja (PTS)',
+    category: 'carnes-ovos-peixes',
+    qtyAvailable: 60,
+    partnerName: 'Mercado 2',
+    availableUntil: new Date().toISOString(),
+  },
+  {
+    inventoryLotId: 15,
+    productName: 'Ovo de codorna',
+    category: 'carnes-ovos-peixes',
+    qtyAvailable: 60,
+    partnerName: 'Mercado 3',
+    availableUntil: new Date().toISOString(),
+  },
+
+  // óleos e gorduras
+  {
+    inventoryLotId: 16,
+    productName: 'Azeite de oliva',
+    category: 'oleos-gorduras',
+    qtyAvailable: 8,
+    partnerName: 'Mercado 1',
+    availableUntil: new Date().toISOString(),
+  },
+  {
+    inventoryLotId: 17,
+    productName: 'Óleo de gergelim',
+    category: 'oleos-gorduras',
+    qtyAvailable: 22,
+    partnerName: 'Mercado 2',
+    availableUntil: new Date().toISOString(),
+  },
+  {
+    inventoryLotId: 18,
+    productName: 'Manteiga',
+    category: 'oleos-gorduras',
+    qtyAvailable: 8,
+    partnerName: 'Mercado 3',
+    availableUntil: new Date().toISOString(),
+  },
+
+  // açucares e doces
+  {
+    inventoryLotId: 19,
+    productName: 'Açúcar mascavo',
+    category: 'acucares-doces',
+    qtyAvailable: 40,
+    partnerName: 'Mercado 1',
+    availableUntil: new Date().toISOString(),
+  },
+  {
+    inventoryLotId: 20,
+    productName: 'Mel',
+    category: 'acucares-doces',
+    qtyAvailable: 14,
+    partnerName: 'Mercado 2',
+    availableUntil: new Date().toISOString(),
+  },
+  {
+    inventoryLotId: 21,
+    productName: 'Melado de cana',
+    category: 'acucares-doces',
+    qtyAvailable: 14,
+    partnerName: 'Mercado 3',
+    availableUntil: new Date().toISOString(),
+  },
+];
+
+/* carboidratos | verduras e legumes | leites e derivados | carnes, ovos e peixes | leguminosas e oleaginosas | óleos e gorduras | açucares e doces */
