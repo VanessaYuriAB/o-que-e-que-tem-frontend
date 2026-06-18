781601a48c86faba66f79eaded75e02b6534023c
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-06-18 15:11:30 -0300

    feat(Menu): enhance layout and update product description sections

--- src/features/menu/pages/Menu.jsx
+++ src/features/menu/pages/Menu.jsx
@@ -15,8 +15,8 @@ function Menu() {
         <h1 className="menu__title"> Será que tem?</h1>
         <h2 className="menu__subtitle">Descubra nossa seleção de ingredientes de hoje. </h2>
         <p className="menu__description">
-          Aqui estão os ingredientes disponíveis. Você pode escolher o que prefere para juntar na
-          sua sopa, creme ou patê.
+          Aqui estão os ingredientes disponíveis, separados por categoria. Você pode escolher o que
+          prefere para juntar na sua sopa, creme ou patê.
         </p>
         <nav className="menu__links nav">
           <ul className="menu__list nav__list">
@@ -31,42 +31,46 @@ function Menu() {
         </nav>
         <Outlet />
       </div>
+
       <aside className="menu__aside">
-        <h3 className="menu__aside-title">Como cada produto é?</h3>
-        <section className="menu__soup">
-          <h4 className="menu__soup-title">Sopa</h4>
-          <p className="menu__soup-description">
-            O caldo é um pouco mais fino, nem tudo é batido, pelo menos um ingrediente é sólido.
-          </p>
-        </section>
-        <section className="menu__cream">
-          <h4 className="menu__cream-title">Creme</h4>
-          <p className="menu__cream-description">
-            Tudo é batido, todos os ingredientes são processados e resultam num creme mais espesso.
-          </p>
-        </section>
-        <section className="menu__pate">
-          <h4 className="menu__pate-title">Patê</h4>
-          <p className="menu__pate-description">É gelado, denso e tudo é batido. </p>
-        </section>
-        <figure className="menu__figure">
-          <img
-            className="menu__figure-img"
-            src={foodPyramidImg}
-            alt="Pirâmide de alimentos com porção diária recomendada."
-          />
-          <figcaption className="menu__figcaption">
-            <a
-              className="menu__figcaption-link"
-              href="https://www.todamateria.com.br/piramide-alimentar"
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label='Ir para site "Toda Matéria", na página do artigo da imagem.'
-            >
-              *https://www.todamateria.com.br/piramide-alimentar
-            </a>
-          </figcaption>
-        </figure>
+        <h3 className="menu__aside-title">Como é cada produto?</h3>
+        <div className="menu__aside-content">
+          <section className="menu__soup">
+            <h4 className="menu__soup-title">Sopa</h4>
+            <p className="menu__soup-description">
+              O caldo é um pouco mais fino, nem tudo é batido, pelo menos um ingrediente é sólido.
+            </p>
+          </section>
+          <section className="menu__cream">
+            <h4 className="menu__cream-title">Creme</h4>
+            <p className="menu__cream-description">
+              Tudo é batido, todos os ingredientes são processados e resultam num creme mais
+              espesso.
+            </p>
+          </section>
+          <section className="menu__pate">
+            <h4 className="menu__pate-title">Patê</h4>
+            <p className="menu__pate-description">É gelado, denso e tudo é batido. </p>
+          </section>
+          <figure className="menu__figure">
+            <img
+              className="menu__figure-img"
+              src={foodPyramidImg}
+              alt="Pirâmide de alimentos com porção diária recomendada."
+            />
+            <figcaption className="menu__figcaption">
+              <a
+                className="menu__figcaption-link"
+                href="https://www.todamateria.com.br/piramide-alimentar"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label='Ir para site "Toda Matéria", na página do artigo da imagem.'
+              >
+                *https://www.todamateria.com.br/piramide-alimentar
+              </a>
+            </figcaption>
+          </figure>
+        </div>
       </aside>
     </section>
   );
