4d5634efaf840196fd4a8c494bafea96fc645003
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-06-24 10:47:50 -0300

    feat(Loader): enable customizable loading message
    Via children prop.

--- src/shared/components/ui/loader/Loader.jsx
+++ src/shared/components/ui/loader/Loader.jsx
@@ -1,11 +1,16 @@
+import PropTypes from 'prop-types';
 import './Loader.css';
 
-function Loader() {
+function Loader({ children }) {
   return (
     <div className="loader" role="status" aria-live="polite">
-      <p className="loader__content">Carregando...</p>
+      <p className="loader__content">{children ? children : 'Carregando...'}</p>
     </div>
   );
 }
+
+Loader.propTypes = {
+  children: PropTypes.string,
+};
 
 export default Loader;
