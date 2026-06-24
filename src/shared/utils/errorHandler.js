a01a6cd1193ca87610293a2a52fb22ded0d152c3
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-06-24 13:54:15 -0300

    feat(ErrorHandler): enhance error responses with scope and status properties

--- src/shared/utils/errorHandler.js
+++ src/shared/utils/errorHandler.js
@@ -2,7 +2,7 @@ export default function errorHandler(error) {
   console.error('Falha no errorHandler:', error.cause ? error.cause : error);
 
   if (error.cause?.type === 'network') {
-    return { message: 'Erro de conexão. Verifique a internet.' };
+    return { message: 'Erro de conexão. Verifique a internet.', scope: 'global' };
   }
 
   if (error.cause?.type === 'api') {
@@ -10,41 +10,60 @@ export default function errorHandler(error) {
       case 400:
         return {
           message: 'Problema no envio dos dados, a solicitação está incorreta.',
+          scope: 'local',
+          status: error.cause.status,
         };
       case 401:
         return {
-          message: 'Não autorizado. Os dados enviados são inválidos ou você não está logado.',
+          message: 'Não autorizado, os dados enviados são inválidos.',
+          scope: 'local',
+          status: error.cause.status,
           action: 'OPEN_LOGIN',
         };
       case 403:
         return {
           message: 'Acesso não autorizado para este recurso.',
+          scope: 'local',
+          status: error.cause.status,
         };
       case 404:
         return {
           message: 'Dado não encontrado, não existe ou foi removido.',
+          scope: 'local',
+          status: error.cause.status,
         };
       case 409:
         return {
           message: 'Usuário já cadastrado.',
+          scope: 'local',
+          status: error.cause.status,
         };
       case 429:
         return {
           message:
             'Foram feitas muitas requisições em um curto período, aguarde um instante para poder prosseguir.',
+          scope: 'local',
+          status: error.cause.status,
         };
       case 500:
         return {
           message: 'Erro interno do servidor. Aguarde um instante e tente novamente.',
+          scope: 'global',
+          status: error.cause.status,
         };
 
       default:
         return {
           message:
-            'Erro ao buscar dados. Problemas no navegador, nas informações enviadas ou houve falha no processamento do servidor.',
+            'Erro ao processar a solicitação. Problemas no navegador, nas informações enviadas ou houve falha no processamento do servidor.',
+          scope: error.cause.status >= 500 ? 'global' : 'local',
+          status: error.cause.status,
         };
     }
   }
 
-  return { message: 'Erro inesperado (desconhecido).' };
+  return {
+    message: 'Erro inesperado (desconhecido).',
+    scope: 'global',
+  };
 }
