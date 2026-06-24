b4c5305c09a8515ca00257991acfebddde886232
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-06-24 12:31:29 -0300

    feat(Loader): add customizable className prop for styling

--- src/shared/components/ui/loader/Loader.jsx
+++ src/shared/components/ui/loader/Loader.jsx
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
 import './Loader.css';
 
-function Loader({ children }) {
+function Loader({ children = '', className = '' }) {
   return (
-    <div className="loader" role="status" aria-live="polite">
+    <div className={`loader ${className}`} role="status" aria-live="polite">
       <p className="loader__content">{children ? children : 'Carregando...'}</p>
     </div>
   );
@@ -11,6 +11,7 @@ function Loader({ children }) {
 
 Loader.propTypes = {
   children: PropTypes.string,
+  className: PropTypes.string,
 };
 
 export default Loader;
