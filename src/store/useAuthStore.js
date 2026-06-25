9296b9ffa3dc22245dbc9f05bc94e34bd7b3e9ff
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-06-25 14:56:58 -0300

    feat(AuthStore): update error handling to set only global errors
    Local errors are simply returned.

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -10,25 +10,35 @@ Retorno da função, não é store
 
 - const handledError = errorHandler(error);
 Acessa error.cause.status internamente
-Converte erro, passando objeto puro para o estado, apenas com msg e action
+Converte erro, passando objeto puro para o estado, contendo: msg, scope, status e action
+
+- if (handledError.scope === 'global')
+Só seta erros globais (status >= 500 || status === 0)
+
+- return { success: false, error: handledError };
+Se for erro local, de api, não seta, apenas retorna
 
 */
 
 const useAuthStore = create((set) => ({
   user: null,
   loading: false,
-  error: null,
+  globalError: null,
 
   // register chama authService.register
   register: async (userData) => {
-    set({ loading: true, error: null });
+    set({ loading: true, globalError: null });
     try {
       await authService.register(userData);
       return { success: true };
     } catch (error) {
       const handledError = errorHandler(error);
-      set({ error: handledError });
-      return { success: false };
+
+      if (handledError.scope === 'global') {
+        set({ globalError: handledError });
+      }
+
+      return { success: false, error: handledError };
     } finally {
       set({ loading: false });
     }
@@ -36,15 +46,19 @@ const useAuthStore = create((set) => ({
 
   // login chama authService.login e define user, ativando login
   login: async (credentials) => {
-    set({ loading: true, error: null });
+    set({ loading: true, globalError: null });
     try {
       const data = await authService.login(credentials);
       set({ user: data });
       return { success: true };
     } catch (error) {
       const handledError = errorHandler(error);
-      set({ error: handledError });
-      return { success: false };
+
+      if (handledError.scope === 'global') {
+        set({ globalError: handledError });
+      }
+
+      return { success: false, error: handledError };
     } finally {
       set({ loading: false });
     }
@@ -52,15 +66,19 @@ const useAuthStore = create((set) => ({
 
   // logout chama authService.logout e reseta user, desativando login
   logout: async () => {
-    set({ loading: true, error: null });
+    set({ loading: true, globalError: null });
     try {
       await authService.logout();
       set({ user: null });
       return { success: true };
     } catch (error) {
       const handledError = errorHandler(error);
-      set({ error: handledError });
-      return { success: false };
+
+      if (handledError.scope === 'global') {
+        set({ globalError: handledError });
+      }
+
+      return { success: false, error: handledError };
     } finally {
       set({ loading: false });
     }
