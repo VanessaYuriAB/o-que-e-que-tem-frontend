d23a00a3979dd6e7d73aee713ecfc3547374ce6c
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-06-27 06:56:52 -0300

    feat(fakeAuthDb): correct property name and structure
    Correct user property name to userName and add confirmEmail and confirmPassword fields.

--- src/mocks/fakeAuthDb.js
+++ src/mocks/fakeAuthDb.js
@@ -3,33 +3,41 @@
 /* Register (success) e Login (401) */
 
 export const User = {
-  name: 'User',
+  userName: 'User',
   email: 'user@email.com',
+  confirmEmail: 'user@email.com',
   tel: '(11) 11111-1111',
   password: 'senha123',
+  confirmPassword: 'senha123',
 };
 
 /* Register (409) e Login (success) */
 
 export const User1 = {
-  name: 'User 1',
+  userNameame: 'User 1',
   email: 'user1@email.com',
+  confirmEmail: 'user1@email.com',
   tel: '(11) 99999-9999',
   password: 'abcd1234',
+  confirmPassword: 'abcd1234',
 };
 
 export const User2 = {
-  name: 'User 2',
+  userName: 'User 2',
   email: 'user2@email.com',
+  confirmEmail: 'user2@email.com',
   tel: '(11) 88888-8888',
   password: 'efgh5678',
+  confirmPassword: 'efgh5678',
 };
 
 export const User3 = {
-  name: 'User 3',
+  userName: 'User 3',
   email: 'user3@email.com',
+  confirmEmail: 'user3@email.com',
   tel: '(11) 77777-7777',
   password: 'ijkl9101112',
+  confirmPassword: 'ijkl9101112',
 };
 
 /* DADOS PARA CONSULTA DE BANCO */
@@ -40,7 +48,7 @@ export const Users = [
   // User 1
   {
     id: 1,
-    name: 'User 1',
+    userName: 'User 1',
     email: 'user1@email.com',
     tel: '(11) 99999-9999',
     password: 'abcd1234',
@@ -49,7 +57,7 @@ export const Users = [
   // User 2
   {
     id: 2,
-    name: 'User 2',
+    userName: 'User 2',
     email: 'user2@email.com',
     tel: '(11) 88888-8888',
     password: 'efgh5678',
@@ -58,7 +66,7 @@ export const Users = [
   // User 3
   {
     id: 3,
-    name: 'User 3',
+    userName: 'User 3',
     email: 'user3@email.com',
     tel: '(11) 77777-7777',
     password: 'ijkl9101112',
