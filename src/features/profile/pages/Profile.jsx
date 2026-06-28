06ca37d7578184298b1da568915692dab49c43f5
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-06-28 07:01:11 -0300

    feat(Profile): update greeting to include user's name

--- src/features/profile/pages/Profile.jsx
+++ src/features/profile/pages/Profile.jsx
@@ -1,9 +1,15 @@
+import useAuthStore from '../../../store/useAuthStore';
+
 import './Profile.css';
 
 function Profile() {
+  const { user } = useAuthStore();
+
+  console.log(user);
+
   return (
     <section>
-      <h1>Perfil</h1>
+      <h1>Olá, {user.userName}</h1>
     </section>
   );
 }
