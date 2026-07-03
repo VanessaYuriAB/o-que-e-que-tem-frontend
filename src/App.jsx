e7630d59f7c2ddb97252d4cf55dca8739fed9dab
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-07-03 11:31:03 -0300

    feat(App): implement user session verification on app load
    Implementing refreshAction.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './app/routes/AppRoutes.jsx';
+import { useEffect } from 'react';
+import useAuthStore from './store/useAuthStore.js';
 
 function App() {
+  const refreshAction = useAuthStore((state) => state.refreshAction);
+
+  useEffect(() => {
+    async function verifyUserSession() {
+      await refreshAction();
+    }
+
+    verifyUserSession();
+  }, [refreshAction]);
+
   return (
     <BrowserRouter>
       <AppRoutes />
