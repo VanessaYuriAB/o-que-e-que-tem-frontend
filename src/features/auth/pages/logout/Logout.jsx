9f7f24ac96d1f5e82e3c68dc61dba485a2089091
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-07-03 13:25:17 -0300

    feat(Logout): improve logout navigation
    Add local error handling, authChecked to verify authentication bootstrapping during disconnection loading and the replace property for navigate.

--- src/features/auth/pages/logout/Logout.jsx
+++ src/features/auth/pages/logout/Logout.jsx
@@ -4,36 +4,43 @@ import Button from '../../../../shared/components/ui/button/Button.jsx';
 import Toast from '../../../../shared/components/ui/toast/Toast.jsx';
 import Loader from '../../../../shared/components/ui/loader/Loader.jsx';
 import { useShallow } from 'zustand/react/shallow';
+import { useState } from 'react';
 
 import './Logout.css';
 
 function Logout() {
-  const { logoutAction, loading, globalError } = useAuthStore(
+  const [localError, setLocalError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const { logoutAction, loading, globalError, authChecked } = useAuthStore(
     useShallow((state) => ({
       logoutAction: state.logoutAction,
       loading: state.loading,
       globalError: state.globalError,
+      authChecked: state.authChecked,
     }))
   );
-
-  const navigate = useNavigate();
 
   const handleLogout = async () => {
     const result = await logoutAction();
 
     if (result.success === true) {
       console.log('deslogado');
-      navigate('/');
+      navigate('/', { replace: true });
+    } else if (result.success === false && result.error.scope === 'local') {
+      setLocalError(result.error.message);
     }
   };
-
-  if (loading) {
-    return <Loader>Desconectando...</Loader>;
-  }
 
   return (
     <section className="logout content__logout">
       <h1 className="logout__title">Você quer desconectar da sua conta?</h1>
+
+      {/* Se estiver em loading de logout, com checagem de autenticação concluída (true) */}
+      {loading && authChecked && <Loader className="logout__loader">Desconectando...</Loader>}
+
+      {localError && <Toast className="logout__toast" message={localError} />}
 
       {globalError && <Toast className="logout__toast" message={globalError.message} />}
 
