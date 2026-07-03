51be00d397cb6a0cf30d8602b58acd29ce6bbf71
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-07-03 11:36:25 -0300

    feat(ProtectedRoute): enhance with loader for refresh
    Implement loading and authChecked states, and Loader component on authentication handling.

--- src/app/routes/ProtectedRoute.jsx
+++ src/app/routes/ProtectedRoute.jsx
@@ -1,9 +1,22 @@
 import PropTypes from 'prop-types';
 import UnauthorizedRoute from '../../pages/unauthorized-route/UnauthorizedRoute.jsx';
 import useAuthStore from '../../store/useAuthStore.js';
+import { useShallow } from 'zustand/react/shallow';
+import Loader from '../../shared/components/ui/loader/Loader.jsx';
 
 function ProtectedRoute({ children }) {
-  const user = useAuthStore((state) => state.user);
+  const { user, loading, authChecked } = useAuthStore(
+    useShallow((state) => ({
+      user: state.user,
+      loading: state.loading,
+      authChecked: state.authChecked,
+    }))
+  );
+
+  // Espera o refresh terminar, com checagem de autenticação em andamento (false)
+  if (loading && !authChecked) {
+    return <Loader>Validando credenciais...</Loader>;
+  }
 
   return user ? children : <UnauthorizedRoute />;
 }
