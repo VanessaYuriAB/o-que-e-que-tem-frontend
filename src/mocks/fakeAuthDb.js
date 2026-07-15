f4043312a6487010a49c0e4c2d4447e12c193960
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-07-14 23:37:05 -0300

    feat(fakeAuthDb): add address details for User1, User2, and User3
    It also correct userName typo, for User1.

--- src/mocks/fakeAuthDb.js
+++ src/mocks/fakeAuthDb.js
@@ -14,12 +14,18 @@ export const User = {
 /* Register (409) e Login (success) */
 
 export const User1 = {
-  userNameame: 'User 1',
+  userName: 'User 1',
   email: 'user1@email.com',
   confirmEmail: 'user1@email.com',
   tel: '(11) 99999-9999',
   password: 'abcd1234',
   confirmPassword: 'abcd1234',
+  address: 'Rua de exemplo para mock 1',
+  number: '1',
+  complement: 'Apto 1',
+  district: 'Bairro mock 1',
+  cep: '99999-999',
+  infoText: 'Infos adicionais mock 1',
 };
 
 export const User2 = {
@@ -29,6 +35,12 @@ export const User2 = {
   tel: '(11) 88888-8888',
   password: 'efgh5678',
   confirmPassword: 'efgh5678',
+  address: 'Rua de exemplo para mock 2',
+  number: '2',
+  complement: 'Apto 2',
+  district: 'Bairro mock 2',
+  cep: '88888-888',
+  infoText: 'Infos adicionais mock 2',
 };
 
 export const User3 = {
@@ -38,6 +50,12 @@ export const User3 = {
   tel: '(11) 77777-7777',
   password: 'ijkl9101112',
   confirmPassword: 'ijkl9101112',
+  address: 'Rua de exemplo para mock 1',
+  number: '3',
+  complement: '',
+  district: 'Bairro mock 3',
+  cep: '77777-777',
+  infoText: '',
 };
 
 /* DADOS PARA CONSULTA DE BANCO */
@@ -53,6 +71,12 @@ export const Users = [
     tel: '(11) 99999-9999',
     password: 'abcd1234',
     role: 'user',
+    address: 'Rua de exemplo para mock 1',
+    number: '1',
+    complement: 'Apto 1',
+    district: 'Bairro mock 1',
+    cep: '99999-999',
+    infoText: 'Infos adicionais mock 1',
   },
   // User 2
   {
@@ -62,6 +86,12 @@ export const Users = [
     tel: '(11) 88888-8888',
     password: 'efgh5678',
     role: 'user',
+    address: 'Rua de exemplo para mock 2',
+    number: '2',
+    complement: 'Apto 2',
+    district: 'Bairro mock 2',
+    cep: '88888-888',
+    infoText: 'Infos adicionais mock 2',
   },
   // User 3
   {
@@ -71,6 +101,12 @@ export const Users = [
     tel: '(11) 77777-7777',
     password: 'ijkl9101112',
     role: 'admin',
+    address: 'Rua de exemplo para mock 1',
+    number: '3',
+    complement: '',
+    district: 'Bairro mock 3',
+    cep: '77777-777',
+    infoText: '',
   },
 ];
 
