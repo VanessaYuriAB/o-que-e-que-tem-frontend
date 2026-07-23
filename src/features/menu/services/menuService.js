faadceb2dab210dc9c2f76270b54604f56c24a30
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-07-23 12:04:41 -0300

    feat(menuService): simulate stock availability updates

--- src/features/menu/services/menuService.js
+++ src/features/menu/services/menuService.js
@@ -11,6 +11,13 @@ export async function getMenu() {
         await fakeApiError('mockFn com err = true no getMenu do menuService'); // Promise.reject envia para o bloco catch
       }
 
+      // Teste de polling: simula mudança no estoque depois que a aplicação já carregou
+      setTimeout(() => {
+        itemsMenu[3].qtyAvailable = 5;
+
+        console.log('Mandioca atualizada para 5');
+      }, 10000);
+
       return await fakeApi({ itemsMenu });
     };
 
