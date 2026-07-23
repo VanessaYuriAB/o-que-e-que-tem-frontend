1bf986d70b6dca8ba69be567db0a741564895759
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-07-23 15:20:22 -0300

    feat(Cart): add empty cart message and styles

--- src/features/cart/pages/Cart.jsx
+++ src/features/cart/pages/Cart.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './Cart.css';
 
 function Cart() {
@@ -5,6 +6,14 @@ function Cart() {
     <section className="cart content__cart">
       <h1 className="cart__title">Carrinho de sopas...</h1>
       <strong className="cart__strong">...cremes ou patês</strong>
+      {/* se houver items selecionados, renderiza carrinho; se não houver, renderiza mensagem */}
+      <div className="cart__null-box">
+        <h2 className="cart__null-title">Está vazio, no momento!</h2>
+        <p className="cart__null-text">Veja o que está disponível em nosso cardápio :)</p>
+        <Link className="cart__null-link link-to-button" to="/menu">
+          Acesse o menu aqui
+        </Link>
+      </div>
     </section>
   );
 }
