92d4e4407f1cb6bd2a31c154c8ae5bfe2d0368af
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-07-23 14:52:46 -0300

    feat(Cart): add component file and associated styles

---
+++ src/features/cart/pages/Cart.jsx
@@ -0,0 +1,12 @@
+import './Cart.css';
+
+function Cart() {
+  return (
+    <section className="cart content__cart">
+      <h1 className="cart__title">Carrinho de sopas...</h1>
+      <strong className="cart__strong">...cremes ou patês</strong>
+    </section>
+  );
+}
+
+export default Cart;
