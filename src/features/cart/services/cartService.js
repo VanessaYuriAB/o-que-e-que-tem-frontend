80f0dfa9f34a005717a03bf81ad6cb0fe5b9f109
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-07-26 12:39:33 -0300

    feat(cartService): add error handling in add and remove item functions

--- src/features/cart/services/cartService.js
+++ src/features/cart/services/cartService.js
@@ -1,17 +1,30 @@
-import { fakeApi } from '../../../shared/utils/fakeApi.js';
+import FAKE_ERRORS from '../../../shared/constants/mockConfig.js';
+import { fakeApi, fakeApiError } from '../../../shared/utils/fakeApi.js';
 
-export function addItemToCart(item) {
+export async function addItemToCart(item) {
+  if (FAKE_ERRORS.addItemToCart) {
+    return fakeApiError(
+      'addItemToCart com err = true no cartService, desconsiderar status e type, pois a persistência do carrinho está configurada apenas no armazenamento local, sem api (por enquanto)'
+    );
+  }
+
   console.log('cartService/addItemToCart:', item);
 
-  const { data } = fakeApi(item, 200);
+  const { data } = fakeApi(item);
 
   return data;
 }
 
-export function removeItemToCart(item) {
+export async function removeItemToCart(item) {
+  if (FAKE_ERRORS.removeItemToCart) {
+    return fakeApiError(
+      'removeItemToCart com err = true no cartService, desconsiderar status e type, pois a persistência do carrinho está configurada apenas no armazenamento local, sem api (por enquanto)'
+    );
+  }
+
   console.log('cartService/removeItemToCart:', item);
 
-  const { data } = fakeApi(item, 200);
+  const { data } = fakeApi(item);
 
   return data;
 }
