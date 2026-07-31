6a696c3e6f2de67c58dfd7375c4cbe3508d26c42
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-07-30 23:34:59 -0300

    fix(checkoutService): update API endpoint from '/orders'

--- src/features/checkout/services/checkoutService.js
+++ src/features/checkout/services/checkoutService.js
@@ -14,7 +14,7 @@ async function sendOrderToServer(order) {
     };
 
     const apiFn = async () => {
-      return await apiFetch('/order', {
+      return await apiFetch('/orders', {
         method: 'POST',
         reqBody: order,
       });
@@ -31,3 +31,65 @@ async function sendOrderToServer(order) {
 }
 
 export default sendOrderToServer;
+
+/*
+
+implementar toast de erro (local + global) e redirecionamento para sucesso com infos do pedido > service + store > order precisa de um id, nº de pedido?
+
+refatorar classes como componente à parte, ex: cart__pack-card-link-box
+
+separar forms de Cart e Checkout, jsx e css?
+
+mockar cart e order?
+
+ajustar botão Subscription em active, no Profile em mobile
+
+refatoração auth, user e subscription (ver roteiro) > renomear refresh() para refreshMe() ou getCurrentUser()
+
+*/
+
+/*
+
+USERPROFILE.JSX e SUBSCRIPTIONPROFILE.JSX
+
+hooks useProfile será usado? ou manter no component? hook para validação de tds os forms (estes + login e register)?
+
+msg de erro, ex: 401, deixar como está no handler, alterar a msg ou repassar a msg do service?
+
+limpar msgs com efeito?
+
+*/
+
+/*
+
+APP.JSX
+
+PROFILE > PARA IMPLEMENTAR INFOS DO USUÁRIO > DEPOIS FAZER REFRESH E TOKEN JWT POR HTTPONLY > erros globais de refresh em Layout? Loading tbm?
+
+IMPLEMENTAR REFRESH COMPLETO > PRIMEIRO AUTHBOOTSTRAP E DEPOIS USERDATA PARA PERFIL? COM HOOKS E EFEITOS INTERNOS? OU APENAS UM EFEITO EM APP.JSX POR CAUSA DO STORE QUE É GLOBAL?
+
+VALIDAÇÃO, RESET E ENVIO DE FORMS > VER ROTEIRO > useform
+
+*/
+
+/*
+
+padronizar em e rem ! em para paddings de componentes pequenos e rem para espaçamentos em blocos maiores? gaps tbm?
+
+padronizar cores (unidade/medida)
+
+padronizar semântica de components reutilizáveis, Button, Loader, Toast > estão em divs?
+
+unificar css para formulários?
+
+*/
+
+/*
+
+REAMDE: "Por se tratar de um MVP para fins educacionais e de portfólio, o processamento de pagamentos não foi integrado a um gateway real. Os campos de pagamento possuem apenas finalidade demonstrativa."
+
+MELHORIA:
+- Cart: botão para finalizar e cadastrar-se, caso ainda não seja cadastrado
+- Refatorar para diversos pedidos em uma order
+
+*/
