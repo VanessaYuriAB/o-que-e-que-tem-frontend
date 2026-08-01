477ffc52004ea8242c91ecc1e41df76cf57a3182
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-08-01 00:09:51 -0300

    feat(OrdersProfile): create skeleton component with CSS file

---
+++ src/features/profile/pages/components/orders-profile/OrdersProfile.jsx
@@ -0,0 +1,11 @@
+import './OrdersProfile.css';
+
+function OrdersProfile() {
+  return (
+    <section>
+      <h3>Histórico de pedidos:</h3>
+    </section>
+  );
+}
+
+export default OrdersProfile;
