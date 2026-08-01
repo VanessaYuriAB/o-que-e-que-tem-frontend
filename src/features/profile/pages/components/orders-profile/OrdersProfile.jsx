005cdc352d60414c4d1fac9ef2ba525353ca1100
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-08-01 18:21:38 -0300

    feat(OrdersProfile): structure component with mock data
    And styling.

--- src/features/profile/pages/components/orders-profile/OrdersProfile.jsx
+++ src/features/profile/pages/components/orders-profile/OrdersProfile.jsx
@@ -1,9 +1,83 @@
 import './OrdersProfile.css';
+import orders from '../../../../../mocks/fakeOrdersDb.js';
 
 function OrdersProfile() {
   return (
-    <section>
-      <h3>Histórico de pedidos:</h3>
+    <section className="profile__orders">
+      <h3 className="profile__orders-title">Histórico de pedidos:</h3>
+      <ul className="profile__orders-list nav__list">
+        {orders.map((order) => {
+          return (
+            <li className="profile__orders-item" key={order._id}>
+              <div className="profile__orders-item-box profile__orders-item-box_center">
+                <p className="profile__orders-label">Nº do pedido:</p>
+                <p className="profile__orders-value">{order.orderNumber}</p>
+              </div>
+              <div className="profile__orders-item-box">
+                <p className="profile__orders-label">Data:</p>
+                <p className="profile__orders-value">
+                  {new Date(order.createdAt).toLocaleString('pt-BR')}
+                </p>
+              </div>
+              <div className="profile__orders-item-box">
+                <p className="profile__orders-label">Forma de entrega:</p>
+                <p className="profile__orders-value">{order.method}</p>
+              </div>
+              {order.method === 'delivery' && (
+                <div className="profile__orders-item-box">
+                  <p className="profile__orders-label">Endereço:</p>
+                  <p className="profile__orders-value">
+                    {order.addressSnapshot.address}, {order.addressSnapshot.number}
+                    {order.addressSnapshot.complement !== '-' &&
+                      `, ${order.addressSnapshot.complement}`}
+                    , {order.addressSnapshot.district}, {order.addressSnapshot.cep}
+                  </p>
+                </div>
+              )}
+              {order.obs && (
+                <div className="profile__orders-item-box">
+                  <p className="profile__orders-label">Informações adicionais:</p>
+                  <p className="profile__orders-value">{order.obs}</p>
+                </div>
+              )}
+              <div className="profile__orders-item-box">
+                <p className="profile__orders-label">Contato:</p>
+                <p className="profile__orders-value">
+                  {order.customerSnapshot.userName} | {order.customerSnapshot.email} |{' '}
+                  {order.customerSnapshot.tel}
+                </p>
+              </div>
+              <div className="profile__orders-item-box">
+                <p className="profile__orders-label">Forma de pagamento:</p>
+                <p className="profile__orders-value">{order.payment}</p>
+              </div>
+              <div className="profile__orders-item-box profile__orders-item-box_inline">
+                <p className="profile__orders-label">R$:</p>
+                <p className="profile__orders-value">{order.amount},00</p>
+              </div>
+              <div className="profile__orders-item-box profile__orders-item-box_inline">
+                <p className="profile__orders-label">Tipo:</p>
+                <p className="profile__orders-value">{order.meal}</p>
+              </div>
+              <div className="profile__orders-item-box">
+                <p className="profile__orders-label">Itens:</p>
+                <ul className="profile__orders-value-list nav__list">
+                  {order.itemsSnapshot.map((item) => {
+                    return (
+                      <li
+                        className="profile__orders-value profile__orders-value_list"
+                        key={item.inventoryLotId}
+                      >
+                        {item.productName}
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </section>
   );
 }
