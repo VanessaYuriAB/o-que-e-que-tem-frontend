4f4401c50dfc15bf196828987bccb51e178c9975
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-07-31 22:52:08 -0300

    feat(SuccessOrder): enhance with classes names
    For order details and empty state.

--- src/pages/success-order/SuccessOrder.jsx
+++ src/pages/success-order/SuccessOrder.jsx
@@ -1,51 +1,90 @@
 import { Link } from 'react-router-dom';
+import './SuccessOrder.css';
 
 function SuccessOrder() {
   const hasOrder = true;
   const user = true;
 
   return (
-    <section>
+    <section className="order content__order">
       {hasOrder ? (
         <>
-          <h1>Pedido enviado com sucesso</h1>
-          <div>
-            <p>
+          <h1 className="order__title">Pedido enviado com sucesso</h1>
+          <div className="order__box">
+            <p className="order__text">
               Logo você pode saborear uma super refeição nutritiva preparada com muito amor e
               carinho s2
             </p>
-            <p>E ainda ajudou a reduzir um pouquinho o desperdício alimentar e o meio ambiente</p>
-            <p>Agradecemos muito :)</p>
+            <p className="order__text">
+              E ainda ajudou a reduzir um pouquinho o desperdício alimentar e o meio ambiente
+            </p>
+            <p className="order__text">Agradecemos muito :)</p>
           </div>
-          <div>
-            <h2>Aqui estão as informações (da sua) (refeição):</h2>
-            <p>Nº do pedido:</p>
-            <p>Forma de entrega:</p>
-            <p>Endereço: (caso delivery)</p>
-            <p>Contato: (nome e telefone, se houver) (alterar, tbm, campo de infos adicionais)</p>
-            <p>Forma de pagamento:</p>
-            <p>Itens:</p>
-            <p>Tipo de refeição:</p>
+
+          <h2 className="order__subtitle">Aqui estão as informações (da sua) (refeição):</h2>
+          <div className="order__details">
+            <div className="order__item-box">
+              <p className="order__item-label">Nº do pedido:</p>
+              <p className="order__item"></p>
+            </div>
+            <div className="order__item-box">
+              <p className="order__item-label">Forma de entrega:</p>
+              <p className="order__item"></p>
+            </div>
+            <div className="order__item-box">
+              <p className="order__item-label">Endereço:</p>
+              <p className="order__item">(caso delivery)</p>
+            </div>
+            <div className="order__item-box">
+              <p className="order__item-label">Contato:</p>
+              <p className="order__item">
+                (nome e telefone, se houver) (alterar, tbm, campo de infos adicionais)
+              </p>
+            </div>
+            <div className="order__item-box">
+              <p className="order__item-label">Forma de pagamento:</p>
+              <p className="order__item"></p>
+            </div>
+            <div className="order__item-box">
+              <p className="order__item-label">Ingredientes:</p>
+              <div className="order__ingredients-box">
+                <p className="order__item"></p>
+              </div>
+            </div>
+            <div className="order__item-box">
+              <p className="order__item-label">Tipo de refeição:</p>
+              <p className="order__item"></p>
+            </div>
           </div>
         </>
       ) : (
         <>
-          <h1>Ops, você não tem um pedido finalizado e enviado agora</h1>
+          <h1 className="order__empty-title">
+            Ops, você não tem um pedido finalizado e enviado agora
+          </h1>
           {user && (
             <>
-              <p>Quer ver seu histórico de pedidos?</p>
-              <Link>Pedidos anteriores</Link>
+              <p className="order__empty-prompt">Quer ver seu histórico de pedidos?</p>
+              <Link className="order__empty-link link-to-button" to="/profile/orders-profile">
+                Pedidos anteriores
+              </Link>
             </>
           )}
 
-          <p>Quer fazer um novo pedido?</p>
-          <Link>Ver cardápio</Link>
+          <p className="order__empty-prompt">Quer fazer um novo pedido?</p>
+          <Link className="order__empty-link link-to-button" to="/menu">
+            Ver cardápio
+          </Link>
 
-          <p>Quer finalizar um pedido em andamento?</p>
-          <Link>Ir para carrinho</Link>
+          <p className="order__empty-prompt">Quer finalizar um pedido em andamento?</p>
+          <Link className="order__empty-link link-to-button" to="/cart">
+            Ir para carrinho
+          </Link>
 
-          <p>Precisa apenas fazer o pagamento?</p>
-          <Link>Ir para checkout</Link>
+          <p className="order__empty-prompt">Precisa apenas fazer o pagamento?</p>
+          <Link className="order__empty-link link-to-button" to="/checkout">
+            Ir para checkout
+          </Link>
         </>
       )}
     </section>
