c4eadb2b4bd7e47c5c3b147c45d529e09895d508
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-08-02 01:29:22 -0300

    style(OrderTracking): implement styling component

--- src/features/orders/pages/order-tracking/OrderTracking.jsx
+++ src/features/orders/pages/order-tracking/OrderTracking.jsx
@@ -5,20 +5,20 @@ import { Outlet } from 'react-router-dom';
 
 function OrderTracking() {
   return (
-    <section>
-      <h1>Quer saber sobre um pedido feito?</h1>
-      <form>
-        <fieldset>
-          <legend>Rastreamento de pedidos:</legend>
-          <div>
-            <label>Nº do pedido:</label>
-            <Input />
+    <section className="tracker tracker__content">
+      <h1 className="tracker__title">Quer saber sobre um pedido feito?</h1>
+      <form className="tracker__form">
+        <fieldset className="tracker__field">
+          <legend className="tracker__legend">Rastreamento de pedidos:</legend>
+          <div className="tracker__input-box">
+            <label className="tracker__label">Nº do pedido:</label>
+            <Input className="tracker__input" />
           </div>
-          <div>
-            <label>e-mail:</label>
-            <Input />
+          <div className="tracker__input-box">
+            <label className="tracker__label">E-mail:</label>
+            <Input className="tracker__input" />
           </div>
-          <Button>Rastrear</Button>
+          <Button className="tracker__button">Rastrear</Button>
         </fieldset>
       </form>
 
