d8590567db0c8bd38a6dec34eb7752c25a6b1b61
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-08-04 09:25:39 -0300

    refactor(OrdersProfile): restructure semantics, css and accessibility

--- src/features/profile/pages/components/orders-profile/OrdersProfile.jsx
+++ src/features/profile/pages/components/orders-profile/OrdersProfile.jsx
@@ -9,71 +9,77 @@ function OrdersProfile() {
         {orders.map((order) => {
           return (
             <li className="profile__orders-item" key={order._id}>
-              <div className="profile__orders-item-box profile__orders-item-box_center">
-                <p className="profile__orders-label">Nº do pedido:</p>
-                <p className="profile__orders-value">{order.orderNumber}</p>
-              </div>
-              <div className="profile__orders-item-box">
-                <p className="profile__orders-label">Data:</p>
-                <p className="profile__orders-value">
-                  {new Date(order.createdAt).toLocaleString('pt-BR')}
-                </p>
-              </div>
-              <div className="profile__orders-item-box">
-                <p className="profile__orders-label">Forma de entrega:</p>
-                <p className="profile__orders-value">{order.method}</p>
-              </div>
-              {order.method === 'delivery' && (
-                <div className="profile__orders-item-box">
-                  <p className="profile__orders-label">Endereço:</p>
-                  <p className="profile__orders-value">
-                    {order.addressSnapshot.address}, {order.addressSnapshot.number}
-                    {order.addressSnapshot.complement !== '-' &&
-                      `, ${order.addressSnapshot.complement}`}
-                    , {order.addressSnapshot.district}, {order.addressSnapshot.cep}
-                  </p>
+              <dl className="profile__orders-details">
+                <div className="profile__orders-item-box profile__orders-item-box_center">
+                  <dt className="profile__orders-term">Nº do pedido:</dt>
+                  <dd className="profile__orders-description">{order.orderNumber}</dd>
                 </div>
-              )}
-              {order.obs && (
                 <div className="profile__orders-item-box">
-                  <p className="profile__orders-label">Informações adicionais:</p>
-                  <p className="profile__orders-value">{order.obs}</p>
+                  <dt className="profile__orders-term">Data:</dt>
+                  <dd className="profile__orders-description">
+                    {new Date(order.createdAt).toLocaleString('pt-BR')}
+                  </dd>
                 </div>
-              )}
-              <div className="profile__orders-item-box">
-                <p className="profile__orders-label">Contato:</p>
-                <p className="profile__orders-value">
-                  {order.customerSnapshot.userName} | {order.customerSnapshot.email} |{' '}
-                  {order.customerSnapshot.tel}
-                </p>
-              </div>
-              <div className="profile__orders-item-box">
-                <p className="profile__orders-label">Forma de pagamento:</p>
-                <p className="profile__orders-value">{order.payment}</p>
-              </div>
-              <div className="profile__orders-item-box profile__orders-item-box_inline">
-                <p className="profile__orders-label">R$:</p>
-                <p className="profile__orders-value">{order.amount},00</p>
-              </div>
-              <div className="profile__orders-item-box profile__orders-item-box_inline">
-                <p className="profile__orders-label">Tipo:</p>
-                <p className="profile__orders-value">{order.meal}</p>
-              </div>
-              <div className="profile__orders-item-box">
-                <p className="profile__orders-label">Itens:</p>
-                <ul className="profile__orders-value-list nav__list">
-                  {order.itemsSnapshot.map((item) => {
-                    return (
-                      <li
-                        className="profile__orders-value profile__orders-value_list"
-                        key={item._id}
-                      >
-                        {item.productName}
-                      </li>
-                    );
-                  })}
-                </ul>
-              </div>
+                <div className="profile__orders-item-box">
+                  <dt className="profile__orders-term">Forma de entrega:</dt>
+                  <dd className="profile__orders-description">{order.method}</dd>
+                </div>
+                {order.method === 'delivery' && (
+                  <div className="profile__orders-item-box">
+                    <dt className="profile__orders-term">Endereço:</dt>
+                    <dd className="profile__orders-description">
+                      {order.addressSnapshot.address}, {order.addressSnapshot.number}
+                      {order.addressSnapshot.complement !== '-' &&
+                        `, ${order.addressSnapshot.complement}`}
+                      , {order.addressSnapshot.district}, {order.addressSnapshot.cep}
+                    </dd>
+                  </div>
+                )}
+                {order.obs && (
+                  <div className="profile__orders-item-box">
+                    <dt className="profile__orders-term">Informações adicionais:</dt>
+                    <dd className="profile__orders-description">{order.obs}</dd>
+                  </div>
+                )}
+                <div className="profile__orders-item-box">
+                  <dt className="profile__orders-term">Contato:</dt>
+                  <dd className="profile__orders-description">
+                    <address className="profile__orders-contact-info">
+                      {order.customerSnapshot.userName} | {order.customerSnapshot.email} |{' '}
+                      {order.customerSnapshot.tel}
+                    </address>
+                  </dd>
+                </div>
+                <div className="profile__orders-item-box">
+                  <dt className="profile__orders-term">Forma de pagamento:</dt>
+                  <dd className="profile__orders-description">{order.payment}</dd>
+                </div>
+                <div className="profile__orders-item-box profile__orders-item-box_inline">
+                  <dt className="profile__orders-term">R$:</dt>
+                  <dd className="profile__orders-description">{order.amount},00</dd>
+                </div>
+                <div className="profile__orders-item-box profile__orders-item-box_inline">
+                  <dt className="profile__orders-term">Tipo:</dt>
+                  <dd className="profile__orders-description">{order.meal}</dd>
+                </div>
+                <div className="profile__orders-item-box">
+                  <dt className="profile__orders-term">Itens:</dt>
+                  <dd className="profile__orders-description">
+                    <ul className="profile__orders-description-list nav__list">
+                      {order.itemsSnapshot.map((item) => {
+                        return (
+                          <li
+                            className="profile__orders-description profile__orders-description_list"
+                            key={item._id}
+                          >
+                            {item.productName}
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  </dd>
+                </div>
+              </dl>
             </li>
           );
         })}
