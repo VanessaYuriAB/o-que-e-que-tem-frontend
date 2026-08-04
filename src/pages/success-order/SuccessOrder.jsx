999da453b773df95773d472ae4c8626bd64300e2
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-08-04 11:10:49 -0300

    refactor(SuccessOrder): enhance semantic structure and accessibility
    It also adjust the styling component.

--- src/pages/success-order/SuccessOrder.jsx
+++ src/pages/success-order/SuccessOrder.jsx
@@ -16,7 +16,7 @@ function SuccessOrder() {
   console.log('hasOrder', hasOrder);
 
   return (
-    <section className="order content__order">
+    <section className="order content__order" aria-live="polite">
       {hasOrder !== null ? (
         <>
           <h1 className="order__title">Pedido enviado com sucesso</h1>
@@ -31,67 +31,73 @@ function SuccessOrder() {
             <p className="order__text">Agradecemos muito :)</p>
           </div>
 
-          <h2 className="order__subtitle">
-            Aqui estão as informações {hasOrder.meal === 'sopa' ? 'da sua' : 'do seu'}{' '}
-            {hasOrder.meal === 'pate' ? 'patê' : hasOrder.meal}:
-          </h2>
-          <div className="order__details">
-            <div className="order__item-box order__item-box_inline">
-              <p className="order__item-label">Nº do pedido:</p>
-              <p className="order__item">{hasOrder.orderNumber}</p>
-            </div>
-            <div className="order__item-box order__item-box_inline">
-              <p className="order__item-label">Forma de entrega:</p>
-              <p className="order__item">{hasOrder.method}</p>
-            </div>
-            {hasOrder.method === 'delivery' && (
-              <div className="order__item-box">
-                <p className="order__item-label">Endereço:</p>
-                <p className="order__item">
-                  {hasOrder.addressSnapshot.address}, {hasOrder.addressSnapshot.number}
-                  {hasOrder.addressSnapshot.complement !== '-' &&
-                    `, ${hasOrder.addressSnapshot.complement}`}
-                  , {hasOrder.addressSnapshot.district}, {hasOrder.addressSnapshot.cep}
-                </p>
+          <article className="order__card">
+            <h2 className="order__subtitle">
+              Aqui estão as informações {hasOrder.meal === 'sopa' ? 'da sua' : 'do seu'}{' '}
+              {hasOrder.meal === 'pate' ? 'patê' : hasOrder.meal}:
+            </h2>
+            <dl className="order__details">
+              <div className="order__detail-box order__detail-box_inline">
+                <dt className="order__term">Nº do pedido:</dt>
+                <dd className="order__description">{hasOrder.orderNumber}</dd>
               </div>
-            )}
-            <div className="order__item-box">
-              <p className="order__item-label">Infos adicionais:</p>
-              <p className="order__item">{hasOrder.obs}</p>
-            </div>
-            <div className="order__item-box">
-              <p className="order__item-label">Contato:</p>
-              <p className="order__item">
-                {hasOrder.customerSnapshot.userName} | {hasOrder.customerSnapshot.email} |{' '}
-                {hasOrder.customerSnapshot.tel}
-              </p>
-            </div>
+              <div className="order__detail-box order__detail-box_inline">
+                <dt className="order__term">Forma de entrega:</dt>
+                <dd className="order__description">{hasOrder.method}</dd>
+              </div>
+              {hasOrder.method === 'delivery' && (
+                <div className="order__detail-box">
+                  <dt className="order__term">Endereço:</dt>
+                  <dd className="order__description">
+                    {hasOrder.addressSnapshot.address}, {hasOrder.addressSnapshot.number}
+                    {hasOrder.addressSnapshot.complement !== '-' &&
+                      `, ${hasOrder.addressSnapshot.complement}`}
+                    , {hasOrder.addressSnapshot.district}, {hasOrder.addressSnapshot.cep}
+                  </dd>
+                </div>
+              )}
+              <div className="order__detail-box">
+                <dt className="order__term">Infos adicionais:</dt>
+                <dd className="order__description">{hasOrder.obs}</dd>
+              </div>
+              <div className="order__detail-box">
+                <dt className="order__term">Contato:</dt>
+                <dd className="order__description">
+                  <address className="order__address">
+                    {hasOrder.customerSnapshot.userName} | {hasOrder.customerSnapshot.email} |{' '}
+                    {hasOrder.customerSnapshot.tel}
+                  </address>
+                </dd>
+              </div>
 
-            <div className="order__item-box order__item-box_inline">
-              <p className="order__item-label">Forma de pagamento:</p>
-              <p className="order__item">{hasOrder.payment}</p>
-            </div>
-            <div className="order__item-box order__item-box_inline">
-              <p className="order__item-label">R$:</p>
-              <p className="order__item">{hasOrder.amount},00</p>
-            </div>
-            <div className="order__item-box order__item-box_inline">
-              <p className="order__item-label">Tipo de refeição:</p>
-              <p className="order__item">{hasOrder.meal}</p>
-            </div>
-            <div className="order__item-box">
-              <p className="order__item-label">Ingredientes:</p>
-              <ul className="order__ingredients-list nav__list">
-                {hasOrder.itemsSnapshot.map((item) => {
-                  return (
-                    <li className="order__item" key={item._id}>
-                      {item.productName}
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-          </div>
+              <div className="order__detail-box order__detail-box_inline">
+                <dt className="order__term">Forma de pagamento:</dt>
+                <dd className="order__description">{hasOrder.payment}</dd>
+              </div>
+              <div className="order__detail-box order__detail-box_inline">
+                <dt className="order__term">R$:</dt>
+                <dd className="order__description">{hasOrder.amount},00</dd>
+              </div>
+              <div className="order__detail-box order__detail-box_inline">
+                <dt className="order__term">Tipo de refeição:</dt>
+                <dd className="order__description">{hasOrder.meal}</dd>
+              </div>
+              <div className="order__detail-box">
+                <dt className="order__term">Ingredientes:</dt>
+                <dd className="order__description">
+                  <ul className="order__ingredients-list nav__list">
+                    {hasOrder.itemsSnapshot.map((item) => {
+                      return (
+                        <li className="order__ingredients-item" key={item._id}>
+                          {item.productName}
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </dd>
+              </div>
+            </dl>
+          </article>
         </>
       ) : (
         <>
