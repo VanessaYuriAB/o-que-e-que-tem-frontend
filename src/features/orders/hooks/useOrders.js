640ac4a285bd648ebec3302d2566882bb39368f9
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-08-05 17:00:53 -0300

    feat(useOrders): add user orders search
    With loading/error states

--- src/features/orders/hooks/useOrders.js
+++ src/features/orders/hooks/useOrders.js
@@ -1,12 +1,17 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import errorHandler from '../../../shared/utils/errorHandler.js';
-import { getOrderByNumber } from '../services/ordersService.js';
+import { getOrderByNumber, getOrderById } from '../services/ordersService.js';
 
 export default function useOrders() {
   const [orderTracked, setOrderTracked] = useState(null);
   const [loadingTracker, setLoadingTracker] = useState(false);
   const [errorTracker, setErrorTracker] = useState(null);
 
+  const [userOrders, setUserOrders] = useState(null);
+  const [loadingProfile, setLoadingProfile] = useState(false);
+  const [errorProfile, setErrorProfile] = useState(null);
+
+  // OrderTracking
   const trackOrder = async (orderData) => {
     setLoadingTracker(true);
     setErrorTracker(null);
@@ -24,5 +29,32 @@ export default function useOrders() {
     }
   };
 
-  return { orderTracked, loadingTracker, errorTracker, trackOrder };
+  // OrdersProfile (consumido em efeito)
+  const getUserOrders = useCallback(async (userId) => {
+    setLoadingProfile(true);
+    setErrorProfile(null);
+
+    try {
+      const orders = await getOrderById(userId);
+      setUserOrders(orders);
+    } catch (error) {
+      const handledError = errorHandler(error);
+
+      setUserOrders(null);
+      setErrorProfile(handledError);
+    } finally {
+      setLoadingProfile(false);
+    }
+  }, []);
+
+  return {
+    orderTracked,
+    loadingTracker,
+    errorTracker,
+    trackOrder,
+    userOrders,
+    loadingProfile,
+    errorProfile,
+    getUserOrders,
+  };
 }
