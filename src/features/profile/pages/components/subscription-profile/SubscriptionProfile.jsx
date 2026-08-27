6f798095702986dbe8f84829ea55a2964acc40ea
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-08-27 16:55:16 -0300

    feat(SubscriptionProfile): calculate next meal date
    Adding getNextDate function based on subscription days.

--- src/features/profile/pages/components/subscription-profile/SubscriptionProfile.jsx
+++ src/features/profile/pages/components/subscription-profile/SubscriptionProfile.jsx
@@ -30,6 +30,33 @@ function SubscriptionProfile() {
     method: user?.subscriptionDetails?.method ?? '',
   });
 
+  const getNextDate = (daysOn, fromDate = new Date()) => {
+    const dayMap = {
+      seg: 1,
+      ter: 2,
+      qua: 3,
+      qui: 4,
+      sex: 5,
+    };
+
+    const validDays = daysOn.map((day) => dayMap[day]);
+
+    for (let i = 0; i < 7; i++) {
+      const date = new Date(fromDate);
+      date.setDate(fromDate.getDate() + i);
+
+      const weekday = date.getDay();
+
+      if (validDays.includes(weekday)) {
+        return date.toISOString().split('T')[0];
+      }
+    }
+
+    return null;
+  };
+
+  const nextMealAt = getNextDate(user.subscriptionDetails.daysOn);
+
   const handleChange = (e) => {
     const { name, value, type, checked } = e.target;
 
@@ -335,7 +362,7 @@ function SubscriptionProfile() {
                   type="date"
                   id="next"
                   name="next"
-                  value={user.subscriptionDetails.next}
+                  value={nextMealAt}
                   disabled
                 />
               </label>
