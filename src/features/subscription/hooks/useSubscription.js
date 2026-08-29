7ace98cc1b38f011c9f3d320b8813e2df7f8a00c
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-08-28 23:30:55 -0300

    feat(useSubscription): add sendSubscribe to handle subscribe
    With user profile update, login, register and error handling.

---
+++ src/features/subscription/hooks/useSubscription.js
@@ -0,0 +1,54 @@
+import errorHandler from '../../../shared/utils/errorHandler.js';
+import { login, register } from '../../auth/services/authService.js';
+import { updateUserProfile } from '../../profile/services/profileService.js';
+import subscribe from '../services/subscriptionService.js';
+import { useState } from 'react';
+
+export default function useSubscription(isUser) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  async function sendSubscribe(data) {
+    setLoading(true);
+    setError(null);
+
+    try {
+      if (isUser) {
+        // Atualiza dados do usuário
+        await updateUserProfile(data);
+        // Envia assinatura
+        await subscribe(data);
+        return;
+      }
+
+      // Se não houver usuário logado
+      // Verifica se já existe cadastro
+      // Loga
+      try {
+        await login(data);
+      } catch (error) {
+        console.log('sendSubscribe', error.cause.status);
+
+        // Se não
+        if (error.cause.status === 401) {
+          // Realiza o cadastro do usuário
+          await register(data);
+          // Depois loga
+          await login(data);
+        }
+      }
+
+      // Para ambos (usuário logado ou não)
+      // Inscreve assinatura
+      await subscribe(data);
+    } catch (error) {
+      const handledError = errorHandler(error);
+      setError(handledError);
+      throw handledError;
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return { sendSubscribe, loading, error };
+}
