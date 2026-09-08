a8f9d92ca45aacc8e558be2574b7377645da4ac8
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-09-08 16:40:15 -0300

    feat(checkoutService): add sendSubscribeOrderToServer function

--- src/features/checkout/services/checkoutService.js
+++ src/features/checkout/services/checkoutService.js
@@ -4,7 +4,7 @@ import { fakeApi, fakeApiError } from '../../../shared/utils/fakeApi.js';
 import decideMockOrApi from '../../../shared/utils/helperMockOrApi.js';
 import useAuthStore from '../../../store/useAuthStore.js';
 
-async function sendOrderToServer(order) {
+export async function sendOrderToServer(order) {
   try {
     const mockFn = async () => {
       const user = useAuthStore.getState().user;
@@ -41,4 +41,41 @@ async function sendOrderToServer(order) {
   }
 }
 
-export default sendOrderToServer;
+export async function sendSubscribeOrderToServer(subscriptionOrder) {
+  try {
+    const mockFn = async () => {
+      const user = useAuthStore.getState().user;
+
+      if (FAKE_ERRORS.sendSubscribeOrderToServer) {
+        await fakeApiError(
+          'mockFn com err = true no sendSubscribeOrderToServer do checkoutService'
+        );
+      }
+
+      const mockSubscriptionOrder = {
+        _id: 'subscription-order-mock',
+        owner: user?._id ?? null,
+        createdAt: new Date().toLocaleString('pt-BR'),
+        subscribeOrderNumber: 'S202609080001',
+        ...subscriptionOrder,
+      };
+
+      return await fakeApi(mockSubscriptionOrder, 201);
+    };
+
+    const apiFn = async () => {
+      return await apiFetch('/subscribe-orders', {
+        method: 'POST',
+        reqBody: subscriptionOrder,
+      });
+    };
+
+    const { data } = await decideMockOrApi(mockFn, apiFn);
+
+    console.log('checkoutService/sendSubscribeOrderToServer:', data);
+
+    return typeof data === 'object' ? data : {};
+  } catch (cause) {
+    throw new Error('Falha no checkoutService.sendSubscribeOrderToServer', { cause });
+  }
+}
