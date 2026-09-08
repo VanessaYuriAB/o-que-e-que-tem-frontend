cee9cdc17e39a1d5a153df575b7192859a505f44
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-09-08 16:41:34 -0300

    feat(useCheckout): add sendOrder and sendSubscribeOrder functions
    To handle services.

---
+++ src/features/checkout/hooks/useCheckout.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { sendOrderToServer, sendSubscribeOrderToServer } from '../services/checkoutService.js';
+import errorHandler from '../../../shared/utils/errorHandler.js';
+
+export default function useCheckout() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  async function sendOrder(order) {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const data = await sendOrderToServer(order);
+      return { success: true, data };
+    } catch (error) {
+      const handledError = errorHandler(error);
+      setError(handledError); // obj puro para o estado, contendo: msg, scope, status e action
+      return { success: false };
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  async function sendSubscribeOrder(subscriptionOrder) {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const data = await sendSubscribeOrderToServer(subscriptionOrder);
+      return { success: true, data };
+    } catch (error) {
+      const handledError = errorHandler(error);
+      setError(handledError); // obj puro para o estado, contendo: msg, scope, status e action
+      return { success: false };
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return { loading, error, sendOrder, sendSubscribeOrder };
+}
