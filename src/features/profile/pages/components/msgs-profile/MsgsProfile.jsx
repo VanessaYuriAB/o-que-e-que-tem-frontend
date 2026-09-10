bf500b1f65937a947ddd0355342cccdab4990939
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-09-10 14:34:37 -0300

    feat(MsgsProfile): implement message list rendering

--- src/features/profile/pages/components/msgs-profile/MsgsProfile.jsx
+++ src/features/profile/pages/components/msgs-profile/MsgsProfile.jsx
@@ -3,16 +3,68 @@ import Toast from '../../../../../shared/components/ui/toast/Toast.jsx';
 import './MsgsProfile.css';
 
 function MsgsProfile() {
-  const hasMsgs = false;
+  const userMsgs = [
+    {
+      _id: '',
+      createdAt: '',
+      userName: '',
+      email: '',
+      whatsapp: '',
+      message: '',
+      method: '',
+      status: true,
+      timeAt: '',
+      response: '',
+    },
+    {},
+  ];
+  const hasMsgs = userMsgs.length > 0;
 
   return (
     <section className="profile__msgs">
       <h3 className="profile__msgs-title">Histórico de mensagens</h3>
 
       {hasMsgs ? (
-        <>
-          <></>
-        </>
+        <ul className="profile__msgs-list nav__list">
+          {userMsgs.map((msg) => (
+            <li className="profile__msgs-item" key={msg._id}>
+              <dl className="profile__msgs-details">
+                <div className="profile__msgs-item-box profile__msgs-item-box_center">
+                  <dt className="profile__msgs-term">Data:</dt>
+                  <dd className="profile__msgs-description">{msg.createdAt}</dd>
+                </div>
+                <div className="profile__msgs-item-box">
+                  <dt className="profile__msgs-term">Mensagem:</dt>
+                  <dd className="profile__msgs-description">{msg.message}</dd>
+                </div>
+                <div className="profile__msgs-item-box">
+                  <dt className="profile__msgs-term">Forma de contato:</dt>
+                  <dd className="profile__msgs-description">
+                    {msg.method === 'email' ? msg.email : msg.whatsapp}
+                  </dd>
+                </div>
+                <div className="profile__msgs-item-box">
+                  <dt className="profile__msgs-term">Status:</dt>
+                  <dd className="profile__msgs-description">
+                    {msg?.status ? 'respondida' : 'pendente'}
+                  </dd>
+                </div>
+                {msg?.status && (
+                  <>
+                    <div className="profile__msgs-item-box">
+                      <dt className="profile__msgs-term">Em:</dt>
+                      <dd className="profile__msgs-description">{msg.timeAt}</dd>
+                    </div>
+                    <div className="profile__msgs-item-box">
+                      <dt className="profile__msgs-term">Resposta:</dt>
+                      <dd className="profile__msgs-description">{msg.response}</dd>
+                    </div>
+                  </>
+                )}
+              </dl>
+            </li>
+          ))}
+        </ul>
       ) : (
         <Toast className="profile__no-msgs-toast">
           <p className="profile__no-msgs-text">Você ainda não nos enviou nenhuma mensagem.</p>
