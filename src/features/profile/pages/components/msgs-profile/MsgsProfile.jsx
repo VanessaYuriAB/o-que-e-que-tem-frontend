bfc67f46e652ce1a02f91160e19fdcfa6c54b929
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-09-10 13:42:00 -0300

    feat(MsgsProfile): add component skeleton and styles file

---
+++ src/features/profile/pages/components/msgs-profile/MsgsProfile.jsx
@@ -0,0 +1,11 @@
+import './MsgsProfile.css';
+
+function MsgsProfile() {
+  return (
+    <section className="profile__msgs">
+      <h3 className="profile__msgs-title">Histórico de mensagens</h3>
+    </section>
+  );
+}
+
+export default MsgsProfile;
