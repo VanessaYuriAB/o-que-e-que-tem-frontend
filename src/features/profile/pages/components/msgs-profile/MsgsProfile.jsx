d8b25ec71b57ba1f8f9d8dcda05549a9f91b58d4
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-09-10 13:57:35 -0300

    feat(MsgsProfile): implement no messages toast
    With navigation link to Contact.

--- src/features/profile/pages/components/msgs-profile/MsgsProfile.jsx
+++ src/features/profile/pages/components/msgs-profile/MsgsProfile.jsx
@@ -1,9 +1,29 @@
+import { Link } from 'react-router-dom';
+import Toast from '../../../../../shared/components/ui/toast/Toast.jsx';
 import './MsgsProfile.css';
 
 function MsgsProfile() {
+  const hasMsgs = false;
+
   return (
     <section className="profile__msgs">
       <h3 className="profile__msgs-title">Histórico de mensagens</h3>
+
+      {hasMsgs ? (
+        <>
+          <></>
+        </>
+      ) : (
+        <Toast className="profile__no-msgs-toast">
+          <p className="profile__no-msgs-text">Você ainda não nos enviou nenhuma mensagem.</p>
+          <p className="profile__no-msgs-text">
+            Pode nos enviar pelo link abaixo, se quiser. Costumamos responder rápido :)
+          </p>
+          <Link className="profile__no-msgs-link link-to-button" to="/talk-to-us">
+            Fale conosco
+          </Link>
+        </Toast>
+      )}
     </section>
   );
 }
