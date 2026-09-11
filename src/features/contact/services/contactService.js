13887e646e95b44a49e00addf3c687ca8aa68c7b
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-09-11 13:15:00 -0300

    feat(contactService): add getMessagesByUserId function with mock data

--- src/features/contact/services/contactService.js
+++ src/features/contact/services/contactService.js
@@ -2,8 +2,9 @@ import decideMockOrApi from '../../../shared/utils/helperMockOrApi';
 import FAKE_ERRORS from '../../../shared/constants/mockConfig.js';
 import { fakeApiError, fakeApi } from '../../../shared/utils/fakeApi.js';
 import apiFetch from '../../../services/api.js';
+import messages from '../../../mocks/fakeMessagesDb.js';
 
-export default async function sendUserMessage(messageData) {
+async function sendUserMessage(messageData) {
   try {
     const mockFn = async () => {
       if (FAKE_ERRORS.sendUserMessage) {
@@ -31,3 +32,33 @@ export default async function sendUserMessage(messageData) {
     throw new Error('Falha no contactService.sendUserMessage', { cause });
   }
 }
+
+async function getMessagesByUserId(userId) {
+  try {
+    const mockFn = async () => {
+      if (FAKE_ERRORS.getMessagesByUserId) {
+        await fakeApiError('mockFn com err = true no getMessagesByUserId do contactService');
+      }
+
+      // Simulação do backend
+      const userMessages = messages.filter((message) => message.owner === userId);
+
+      // Caso não existam mensagens o retorno é []; não é um erro, e é direcionado no próprio componente
+
+      return await fakeApi(userMessages);
+    };
+
+    const apiFn = async () => {
+      return await apiFetch(`/messages/:${userId}`);
+    };
+
+    const { data } = await decideMockOrApi(mockFn, apiFn);
+
+    console.log('getMessagesByUserId', data);
+    return Array.isArray(data) ? data : [];
+  } catch (cause) {
+    throw new Error('Falha no contactService.getMessagesByUserId', { cause });
+  }
+}
+
+export { sendUserMessage, getMessagesByUserId };
