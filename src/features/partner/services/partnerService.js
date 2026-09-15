c53101c32fde0fde41bd07e181b6b74851e964f0
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-09-15 14:10:02 -0300

    feat(partnerService): add sendPartnerEntry function
    For partner data submission.

---
+++ src/features/partner/services/partnerService.js
@@ -0,0 +1,33 @@
+import FAKE_ERRORS from '../../../shared/constants/mockConfig.js';
+import { fakeApiError, fakeApi } from '../../../shared/utils/fakeApi.js';
+import apiFetch from '../../../services/api.js';
+import decideMockOrApi from '../../../shared/utils/helperMockOrApi.js';
+
+async function sendPartnerEntry(partnerData) {
+  try {
+    const mockFn = async () => {
+      if (FAKE_ERRORS.sendPartnerEntry) {
+        await fakeApiError('mockFn com err = true no sendPartnerEntry do partnerService');
+      }
+
+      return await fakeApi(partnerData, 201);
+    };
+
+    const apiFn = async () => {
+      return await apiFetch('/partners', {
+        method: 'POST',
+        // header já definido em apiFetch
+        reqBody: partnerData,
+      });
+    };
+
+    const { data } = await decideMockOrApi(mockFn, apiFn);
+
+    console.log('sendPartnerEntry', data);
+    return typeof data === 'object' ? data : {};
+  } catch (cause) {
+    throw new Error('Falha no partnerService.sendPartnerEntry', { cause });
+  }
+}
+
+export default sendPartnerEntry;
