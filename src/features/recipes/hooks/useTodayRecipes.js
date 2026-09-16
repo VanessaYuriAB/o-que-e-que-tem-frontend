e5262e505dafc1b611c2dc4a1bf71082a0f95b82
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-09-16 15:59:39 -0300

    feat(useTodayRecipes): create custom hook to fetch and cache today's recipes

---
+++ src/features/recipes/hooks/useTodayRecipes.js
@@ -0,0 +1,54 @@
+import { useState, useEffect } from 'react';
+import { getTodayRecipes } from '../services/recipesService.js';
+import errorHandler from '../../../shared/utils/errorHandler.js';
+
+export default function useTodayRecipes() {
+  const [todayRecipes, setTodayRecipes] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function loadTodayRecipes() {
+      try {
+        const cached = localStorage.getItem('todayRecipes');
+
+        if (cached) {
+          const { date, recipes } = JSON.parse(cached);
+
+          const today = new Date().toISOString().slice(0, 10);
+
+          const cacheIsValid = date === today && Array.isArray(recipes) && recipes.length > 0;
+
+          if (cacheIsValid) {
+            setTodayRecipes(recipes);
+            return;
+          }
+
+          // Se o cache não for válido
+          localStorage.removeItem('todayRecipes');
+        }
+
+        setLoading(true);
+        setError(null);
+
+        const data = await getTodayRecipes();
+
+        localStorage.setItem(
+          'todayRecipes',
+          JSON.stringify({ date: new Date().toISOString().slice(0, 10), recipes: data })
+        );
+
+        setTodayRecipes(data);
+      } catch (error) {
+        const handledError = errorHandler(error);
+        setError(handledError);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    loadTodayRecipes();
+  }, []);
+
+  return { todayRecipes, loading, error };
+}
