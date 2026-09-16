1721f21ea2cda4a48fbfda2c8c3f9f5887371308
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-09-16 15:56:28 -0300

    feat(fakeTodayRecipesDb): add mock data for today's recipes

---
+++ src/mocks/fakeTodayRecipesDb.js
@@ -0,0 +1,7 @@
+const todayRecipesDb = [
+  { id: 1, name: 'Sopa', ingredients: [], preparation: '' },
+  { id: 2, name: 'Creme', ingredients: [], preparation: '' },
+  { id: 3, name: 'Patê', ingredients: [], preparation: '' },
+];
+
+export default todayRecipesDb;
