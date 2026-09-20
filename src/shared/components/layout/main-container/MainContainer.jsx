3f148ddcaa01d87ae8006ce9b1948cb2f556e8dc
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2026-09-19 23:00:52 -0300

    feat(MainContainer): add error handling for session and cart synchronization

--- src/shared/components/layout/main-container/MainContainer.jsx
+++ src/shared/components/layout/main-container/MainContainer.jsx
@@ -1,10 +1,29 @@
 import { Outlet } from 'react-router-dom';
-
+import useAuthStore from '../../../../store/useAuthStore.js';
+import Toast from '../../ui/toast/Toast.jsx';
+import useCartStore from '../../../../store/useCartStore.js';
 import './MainContainer.css';
 
 function MainContainer() {
+  const refreshError = useAuthStore((state) => state.refreshError);
+  const syncCartError = useCartStore((state) => state.syncCartError);
+
   return (
     <main className="content page__content">
+      {refreshError && (
+        <Toast
+          className="content__toast"
+          message={`Não foi possível verificar a sua sessão. ${refreshError.message}`}
+        />
+      )}
+
+      {syncCartError && (
+        <Toast
+          className="content__toast"
+          message="Não foi possível ajustar os dados para o carrinho. Algumas funcionalidades de compra podem ficar indisponíveis. Tente recarregar a página."
+        />
+      )}
+
       <Outlet />
     </main>
   );
